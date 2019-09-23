64377bd3b86519afac80fff6b44f784bf433a45f
liamgbs <liam.gibbings@missionlabs.co.uk>
2019-09-23 15:11:28 +0100

    Added canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Canvas from './Canvas';
 
 const App: React.FC = () => {
     return (
         <div className="app">
-            
+            <Canvas />
         </div>
     );
 }
---
+++ src/Canvas.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Canvas.scss';
+
+
+const Canvas: React.FC = () => {
+    const [val, setVal] = useState("")
+    return (
+        <div className="canvas">
+
+        </div>
+    )
+}
+
+const _: React.FC = () => {
+    return null;
+}
+
+export default Canvas
